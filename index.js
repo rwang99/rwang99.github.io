5ba76a42510183e23e2f062c28c3447e4beacf0e
Richard Wang <rkwang@umd.edu>
2017-12-24 00:35:38 -0500

    CSS Experimentation

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express();
 
+router.use(express.static('public'));
 var path = require('path');
 
 router.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname+'/index.html'));
+	 res.sendFile(path.join(__dirname+'/view/index.html'));
+
 });
 
 router.listen(3000, function () {
   console.log('Website listening on port 3000!');
 });
 
-console.log('tis running');
