d18806800fb030d9a05ca1db717fec8f40df56d3
Richard Wang <rkwang@umd.edu>
2017-12-28 21:10:54 -0500

    Smooth scrolling!

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -3,5 +3,23 @@ $(function () {
     var $nav = $(".navbar-fixed-top");
     $nav.toggleClass('scrolled', $(this).scrollTop() > 300);
   });
+
+  $(document).ready(function(){
+  	// Get height of header
+  	var headerHeight = $('.navbar-fixed-top').outerHeight();
+
+	$('.smooth').click(function(e){
+		
+		var linkHref = $(this).attr('href');	// Grabs the link
+
+		$('html, body').animate({
+			scrollTop: $(linkHref).offset().top - headerHeight
+		}, 1000); 
+
+		e.preventDefault();			// Prevents automatic switching
+	});
 });
+});
+
+
 
