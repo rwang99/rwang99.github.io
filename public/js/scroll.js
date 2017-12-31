5523a4ecaa63a568ce8baa83c9e5b279a2dddbe5
Richard Wang <rkwang@umd.edu>
2017-12-30 20:46:48 -0500

    Flipping cards

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,17 +1,30 @@
 $(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar-fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > 300);	//Gives solid background to navbar after 300px
-  });
+	$(document).scroll(function () {
+		var $nav = $(".navbar-fixed-top");
+    	$nav.toggleClass('scrolled', $(this).scrollTop() > 300);	//Gives solid background to navbar after 300px
+		
 
-  $(document).ready(function(){
+		var $elem = $('#display-card');
+		$elem.toggleClass('flip', $(this).scrollTop()+$(window).height() - 50 > $elem.offset().top);
+    });
+
+	function checkAnimation() {
+	    
+	    console.log($elem);
+	    // If the animation has already been started
+	    if ($elem.hasClass('flip')) return;
+
+	    if (isElementInViewport($elem)) {
+	        $elem.addClass('flip');
+	    }
+	}
+
+	$(document).ready(function(){
   	// Get height of header
   	var headerHeight = $('.navbar-fixed-top').outerHeight();
 
-	$('.smooth').click(function(e){
-		
+  	$('.smooth').click(function(e){
 		var linkHref = $(this).attr('href');	// Grabs the link
-
 		$('html, body').animate({
 			scrollTop: $(linkHref).offset().top - headerHeight
 		}, 1000); 
@@ -19,7 +32,14 @@ $(function () {
 		window.location.hash = linkHref;		// Updates url
 		e.preventDefault();			// Prevents automatic switching
 	});
-});
+
+  	$('.flipper').click(function(){
+  		$("#display-card").toggleClass('flip');
+  	});
+
+
+  });
+
 });
 
 
